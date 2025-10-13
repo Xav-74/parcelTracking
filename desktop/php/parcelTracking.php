5f6843abe454cda8fd6a3517df450390133850fb
Mathieu <34892335+OUARZA@users.noreply.github.com>
2025-10-13 20:06:27 +0200

    Update parcelTracking.php
    Français puis les autres
    + tri alphabétique

--- desktop/php/parcelTracking.php
+++ desktop/php/parcelTracking.php
@@ -17,7 +17,6 @@ $eqLogics = eqLogic::byType($plugin->getId());
 	<div class="col-xs-12 eqLogicThumbnailDisplay">
 		
 		<legend><i class="fas fa-cog"></i> {{Gestion}}</legend>
-		<!-- Boutons de gestion du plugin -->
 		<div class="eqLogicThumbnailContainer">
 			
 			<div class="cursor eqLogicAction logoPrimary" style="color:#3CA5F0" data-action="add">
@@ -32,9 +31,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<span>{{Configuration}}</span>
 			</div>
 
-			<!--Bouton Community-->
 			<?php
-				// uniquement si on est en version 4.4 ou supérieur
 				$jeedomVersion  = jeedom::version() ?? '0';
 				$displayInfoValue = version_compare($jeedomVersion, '4.4.0', '>=');
 				if ($displayInfoValue) {
@@ -51,7 +48,6 @@ $eqLogics = eqLogic::byType($plugin->getId());
 		if (count($eqLogics) == 0) {
 			echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement Template trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
 		} else {
-			// Champ de recherche
 			echo '<div class="input-group" style="margin:5px;">';
 			echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic">';
 			echo '<div class="input-group-btn">';
@@ -59,18 +55,16 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			echo '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
 			echo '</div>';
 			echo '</div>';
-			// Liste des équipements du plugin
 			echo '<legend><i class="fas fa-box"></i> {{Mes colis}}</legend>';
 			echo '<div class="eqLogicThumbnailContainer">';
 			foreach ($eqLogics as $eqLogic) {
-				if ( $eqLogic->getConfiguration('eqLogicType') == 'parcel' || $eqLogic->getConfiguration('eqLogicType') == '' ) {
+				if ($eqLogic->getConfiguration('eqLogicType') == 'parcel' || $eqLogic->getConfiguration('eqLogicType') == '') {
 					$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
 					echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
 					echo '<img src="' . $eqLogic->getImage() . '"/>';
 					echo '<br>';
 					echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
 					echo '<span class="hiddenAsCard displayTableRight hidden">';
-					//echo ($eqLogic->getIsVisible() == 1) ? '<i class="fas fa-eye" title="{{Equipement visible}}"></i>' : '<i class="fas fa-eye-slash" title="{{Equipement non visible}}"></i>';
 					echo '</span>';
 					echo '</div>';
 				}
@@ -81,18 +75,16 @@ $eqLogics = eqLogic::byType($plugin->getId());
 		if (count($eqLogics) == 0) {
 			echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement Template trouvé, cliquer sur "Ajouter" pour commencer}}</div>';
 		} else {
-			// Liste des widgets du plugin
 			echo '<legend><i class="fas fa-palette"></i> {{Widget dédié géré par le plugin}}</legend>';
 			echo '<div class="eqLogicThumbnailContainer">';
 			foreach ($eqLogics as $eqLogic) {
-				if ( $eqLogic->getConfiguration('eqLogicType') == 'global' ) {	
+				if ($eqLogic->getConfiguration('eqLogicType') == 'global') {	
 					$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
 					echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
 					echo '<img src="' . $eqLogic->getImage() . '"/>';
 					echo '<br>';
 					echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
 					echo '<span class="hiddenAsCard displayTableRight hidden">';
-					//echo ($eqLogic->getIsVisible() == 1) ? '<i class="fas fa-eye" title="{{Equipement visible}}"></i>' : '<i class="fas fa-eye-slash" title="{{Equipement non visible}}"></i>';
 					echo '</span>';
 					echo '</div>';
 				}
@@ -100,16 +92,12 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			echo '</div>';
 		}
 		?>
-	</div> <!-- /.eqLogicThumbnailDisplay -->
+	</div>
 
-
-	<!-- Page de présentation de l'équipement -->
 	<div class="col-xs-12 eqLogic" style="display: none;">
 		
-	<!-- barre de gestion de l'équipement -->
 		<div class="input-group pull-right" style="display:inline-flex;">
 			<span class="input-group-btn">
-				<!-- Les balises <a></a> sont volontairement fermées à la ligne suivante pour éviter les espaces entre les boutons. Ne pas modifier -->
 				<a class="btn btn-sm btn-default eqLogicAction roundedLeft" data-action="configure"><i class="fas fa-cogs"></i><span class="hidden-xs"> {{Configuration avancée}}</span>
 				</a><a class="btn btn-sm btn-default eqLogicAction" data-action="copy"><i class="fas fa-copy"></i><span class="hidden-xs"> {{Dupliquer}}</span>
 				</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}
@@ -118,26 +106,22 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			</span>
 		</div>
 
-		<!-- Onglets -->
 		<ul class="nav nav-tabs" role="tablist">
 			<li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
 			<li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
 			<li role="presentation"><a href="#commandtab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-list"></i> {{Commandes}}</a></li>
 		</ul>
-		
+
 		<div class="tab-content">
-			
-			<!-- Onglet de configuration de l'équipement -->
+
 			<div role="tabpanel" class="tab-pane active" id="eqlogictab">
-				
+
 				<form class="form-horizontal">
 					<fieldset>
-						
-						<!-- Partie gauche de l'onglet "Equipements" -->
-						<!-- Paramètres généraux et spécifiques de l'équipement -->
+
 						<div class="col-lg-6">
 							<legend><i class="fas fa-wrench"></i> {{Paramètres généraux}}</legend>
-							
+
 							<div class="form-group">
 								<label class="col-sm-4 control-label">{{Nom de l'équipement}}</label>
 								<div class="col-sm-6">
@@ -145,7 +129,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement}}">
 								</div>
 							</div>
-							
+
 							<div class="form-group">
 								<label class="col-sm-4 control-label">{{Objet parent}}</label>
 								<div class="col-sm-6">
@@ -161,7 +145,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 									</select>
 								</div>
 							</div>
-							
+
 							<div class="form-group">
 								<label class="col-sm-4 control-label">{{Catégorie}}</label>
 								<div class="col-sm-6">
@@ -174,7 +158,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 									?>
 								</div>
 							</div>
-							
+
 							<div class="form-group">
 								<label class="col-sm-4 control-label">{{Options}}</label>
 								<div class="col-sm-6">
@@ -184,7 +168,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 							</div>
 
 							<legend id="parcel"><i class="fas fa-cogs"></i> {{Paramètres spécifiques}}</legend>
-							
+
 							<div class="form-group" style="display: none;">
 								<label class="col-sm-4 control-label">{{Type équipement}}</label>
 								<div class="col-sm-6">
@@ -216,8 +200,30 @@ $eqLogics = eqLogic::byType($plugin->getId());
 												$json = file_get_contents('plugins/parcelTracking/data/apicarrier.all.json');
 												$carriers = json_decode($json, true);
 
-												foreach($carriers as $carrier) {
-													echo '<option value="'.$carrier['key'].'">'.$carrier['_name'].'</option>"';
+												$french = array_filter($carriers, function($c) {
+													return isset($c['_country_iso']) && strtoupper($c['_country_iso']) === 'FR';
+												});
+												$others = array_filter($carriers, function($c) {
+													return !isset($c['_country_iso']) || strtoupper($c['_country_iso']) !== 'FR';
+												});
+
+												usort($french, function($a, $b) {
+													return strcasecmp($a['_name'], $b['_name']);
+												});
+												usort($others, function($a, $b) {
+													return strcasecmp($a['_name'], $b['_name']);
+												});
+
+												foreach ($french as $carrier) {
+													echo '<option value="' . $carrier['key'] . '">' . htmlspecialchars($carrier['_name']) . '</option>';
+												}
+
+												if (count($french) > 0 && count($others) > 0) {
+													echo '<option disabled>--------------------</option>';
+												}
+
+												foreach ($others as $carrier) {
+													echo '<option value="' . $carrier['key'] . '">' . htmlspecialchars($carrier['_name']) . '</option>';
 												}
 											?>
 										</select>
@@ -249,11 +255,9 @@ $eqLogics = eqLogic::byType($plugin->getId());
 										<a class="btn btn-primary btn-sm cmdAction" id="bt_register"><i class="fas fa-save"></i> {{Enregistrement}}</a>
 									</div>	
 							</div>
-					
+
 						</div>
 
-						<!-- Partie droite de l'onglet "Équipement" -->
-						<!-- Affiche un champ de commentaire par défaut mais vous pouvez y mettre ce que vous voulez -->
 						<div class="col-lg-6">
 							
 							<legend id="parcel"><i class="fas fa-info"></i> {{Informations}}</legend>
@@ -269,12 +273,9 @@ $eqLogics = eqLogic::byType($plugin->getId());
 
 					</fieldset>
 				</form>
-			</div><!-- /.tabpanel #eqlogictab-->
+			</div>
 
-			<!-- Onglet des commandes de l'équipement -->
 			<div role="tabpanel" class="tab-pane" id="commandtab">
-				<!--<a class="btn btn-default btn-sm pull-right cmdAction" data-action="add" style="margin-top:5px;"><i class="fas fa-plus-circle"></i> {{Ajouter une commande}}</a>
-				<br><br>-->
 				<div class="table-responsive">
 					<table id="table_cmd" class="table table-bordered table-condensed">
 						<thead>
@@ -292,16 +293,13 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						</tbody>
 					</table>
 				</div>
-			</div><!-- /.tabpanel #commandtab-->
+			</div>
 
-		</div><!-- /.tab-content -->
+		</div>
 
-	</div><!-- /.eqLogic -->
+	</div>
 
-</div><!-- /.row row-overflow -->
+</div>
 
-
-<!-- Inclusion du fichier javascript du plugin (dossier, nom_du_fichier, extension_du_fichier, id_du_plugin) -->
 <?php include_file('desktop', 'parcelTracking', 'js', 'parcelTracking'); ?>
-<!-- Inclusion du fichier javascript du core - NE PAS MODIFIER NI SUPPRIMER -->
 <?php include_file('core', 'plugin.template', 'js'); ?>
